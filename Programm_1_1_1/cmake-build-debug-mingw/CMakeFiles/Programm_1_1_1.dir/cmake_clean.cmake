68855a41c72454c5ae1148b41e102d09ced9e23c
Altar327 <altar327@gmail.com>
2021-10-07 00:31:48 +0700

    Prog_1_1_1

---
+++ Programm_1_1_1/cmake-build-debug-mingw/CMakeFiles/Programm_1_1_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Programm_1_1_1.dir/main.cpp.obj"
+  "Programm_1_1_1.exe"
+  "Programm_1_1_1.exe.manifest"
+  "Programm_1_1_1.pdb"
+  "libProgramm_1_1_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Programm_1_1_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Programm_1_1_1/main.cpp
@@ -0,0 +1,226 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+
+using namespace std;
+
+void _swap (int& a, int& b) {
+    int c = a;
+    a = b;
+    b = c;
+}
+
+class matrix {
+    int n;                  //Размерность матрицы
+    int **A;                //Сама матрица
+public:
+    matrix() : n(0), A(nullptr) {}
+
+    matrix(int N) : n(N) {
+        A = new int *[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = new int [n];
+            for (int j = 0; j < n; j++) {
+                cin >> A[i][j];
+            }
+        }
+    };
+
+    matrix& operator=(const matrix& inst) {
+        n = inst.n;
+        A = new int *[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = new int [n];
+            for (int j = 0; j < n; j++) {
+                A[i][j] = inst.A[i][j];
+            }
+        }
+        return *this;
+    };
+
+    matrix(int N, int k) : n(N) {
+        A = new int *[n];
+        for (int i = 0; i < n; i++) {
+            A[i] = new int [n];
+            for (int j = 0; j < n; j++) {
+                if (j == i) {
+                    A[i][j] = k;
+                } else {
+                    A[i][j] = 0;
+                }
+            }
+        }
+    };
+
+    matrix(int N, int **values) : n(N) {
+        this->A = values;
+    }
+
+//    matrix(int N1, int N2, int **values) {                //Конструктор матрицы размерности n1 на n2
+//        A = new int *[N1];
+//        for (int i = 0; i < N1; i++) {
+//            A[i] = new int [n];
+//            for (int j = 0; j < N2; j++) {
+//                cin >> A[i][j];
+//            }
+//        }
+//    };
+
+    int get_n() const {
+        return n;
+    }
+
+    int get_count(int i, int j) const {
+        return A[i][j];
+    }
+
+    void print_matrix() const {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << A[i][j] << ' ';
+            }
+            cout << endl;
+        }
+    };
+
+    void fout_matrix() const {
+        ofstream name_file;
+        name_file.open("test");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                name_file << A[i][j];
+            }
+            name_file << endl;
+//            cout << '\n';
+        }
+    };
+
+//    matrix (const matrix& orig) {
+//        this->numerator = orig.numerator;
+//
+//        printf("copied!");
+//    }
+
+
+    ~matrix() {
+        delete[] A;
+    };
+
+
+    matrix operator +(const matrix &m2) {
+        if (n != m2.n) {
+            cout << "Невозможно сложить матрицы разной размерности";
+            return matrix();
+        }
+        matrix m_sum = matrix(n, 0);
+        int **S = new int *[n];
+        for (int i = 0; i < n; i++) {
+            S[i] = new int [n];
+            for (int j = 0; j < n; j++) {
+                S[i][j] = A[i][j] + m2.A[i][j];
+            }
+        }
+        return matrix(n, S);
+    }
+
+    matrix operator - (const matrix &m2) {
+        if (n != m2.n) {
+            cout << "Невозможно вычесть матрицы разной размерности";
+            return matrix();
+        }
+        int **S = new int *[n];
+        for (int i = 0; i < n; i++) {
+            S[i] = new int [n];
+            for (int j = 0; j < n; j++) {
+                S[i][j] = A[i][j] - m2.get_count(i, j);
+            }
+        }
+        return matrix(n, S);
+    }
+
+    matrix operator* (const matrix &m2) {
+        if (n != m2.n) {
+            cout << "Я всего лишь машина откуда мне знать как это умножить";
+            return matrix();
+        }
+        int **S = new int *[n];
+        for (int i = 0; i < n; i++) {
+            S[i] = new int [n];
+            for (int j = 0; j < n; j++) {
+                S[i][j] = 0;
+                for (int f = 0; f < n; f++) {
+                    S[i][j] += A[i][f] * m2.A[f][j];
+                }
+            }
+        }
+        return matrix(n, S);;
+    }
+
+
+    matrix& operator ! () {            //Транспонирование матрицы
+        //int **S = new int *[n];
+        for (int i = 0; i < n; i++) {
+            //S = new int *[n];
+            for (int j = 0; j < i; j++) {
+                _swap(A[i][j], A[j][i]);
+            }
+        }
+        return *this;
+    }
+
+    matrix operator == (const matrix &m2) {
+        if (n != m2.get_n()) {
+            cout << "Я всего лишь машина откуда мне знать как это сравнивать";
+            return matrix();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (A[i][j] != m2.A[i][j]) {
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+
+    matrix operator != (const matrix &m2) {
+        if (n != m2.n) {
+            cout << "Я всего лишь машина откуда мне знать как это сравнивать";
+            cout << "Press F to pay respect";
+            return matrix();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (A[i][j] != m2.A[i][j]) {
+                    return 1;
+                }
+            }
+        }
+        return 0;
+    }
+
+};
+//matrix operator () (int a, int b) {
+//
+//}
+
+int main() {
+    int n, k;
+    cin >> n;
+    cin >> k;
+    matrix A = matrix(n);
+    matrix B = matrix(n);
+    matrix C = matrix(n);
+    matrix D = matrix(n);
+    matrix K = matrix(n, k);
+
+    ((A + (B * !C) + K) * (!D)).print_matrix();
+//    !C;
+//    !D;
+//    B * C;
+//    A + B;
+//    A + K;
+//    A * D;
+//    A.print_matrix();
+    return 0;
+}
